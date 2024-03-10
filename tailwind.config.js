4e326f9b11b492dbd28257740f8990e812e1f127
HeitorCand <heitor.candido@sou.inteli.edu.br>
2024-03-09 22:01:08 -0300

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: [
     "./index.html",
     "./index.css",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/assets/",
+    "./src/**/*.{js,ts,jsx,tsx,svg}",
   ],
   theme: {
     extend: {
